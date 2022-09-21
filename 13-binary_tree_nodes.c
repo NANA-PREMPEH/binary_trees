0e906b5b1d8ea62bf1bff7ea11482b35cc8bc8a7
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2022-09-21 01:28:49 -0700

    tree nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: number of nodes with at least 1 child
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int node, node1;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+
+	node = binary_tree_nodes(tree->left);
+	node1 = binary_tree_nodes(tree->right);
+
+	return (node + 1 + node1);
+}
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == 0 && node->right == 0)
+		return (1);
+
+	else
+		return (0);
+}
