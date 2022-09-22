8832b0c8b341a67636d47f3b47a2397a97b7a2a7
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2022-09-22 08:09:29 -0700

    tree level order

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	int height;
+	int i;
+
+	height = binary_tree_height(tree);
+
+	for (i = 1; i <= height; i++)
+	{
+		binary_tree_levelorder(tree->left, func);
+		func(tree->n);
+		binary_tree_levelorder(tree->rght, func);
+		func(tree->n);
+	}
+}
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int lft, rght;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+
+	lft = binary_tree_height(tree->left);
+	rght = binary_tree_height(tree->right);
+
+	if (lft > rght)
+		return (lft + 1);
+	else
+		return (rght + 1);
+}
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == 0 && node->rght == 0)
+		return (1);
+
+	else
+		return (0);
+}
